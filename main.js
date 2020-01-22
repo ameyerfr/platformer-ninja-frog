0e2f240fff6efce46523f2936f331a66d049287d
Alex Meyer <alexandre.meyer78@gmail.com>
2020-01-22 12:26:01 +0100

    Add tiles to map

--- main.js
+++ main.js
@@ -38,10 +38,16 @@ window.onload = function() {
     {id:4, type:'hardblock-h',  left:192, bottom:184},
     {id:5, type:'hardblock-h',  left:288, bottom:184},
     {id:6, type:'hardblock-h',  left:384, bottom:184},
-    {id:6, type:'hardblock-h',  left:384, bottom:184},
-    {id:6, type:'hardblock-32',  left:608, bottom:184},
-    {id:99, type:'platform',     h:10, w:96, left:300, bottom:350, animate:{keys:[{transform:'translateX(-50px)'},{transform:'translateX(50px)'}], duration:2000} },
-    {id:100, type:'platform',     h:10, w:96, left:550, bottom:350, animate:{keys:[{transform:'translateX(50px)'},{transform:'translateX(-50px)'}], duration:2000} }
+    {id:7, type:'hardblock-h',  left:384, bottom:184},
+    {id:8, type:'hardblock-32',  left:608, bottom:184},
+    {id:9, type:'hardblock-h',  left:0, bottom:350},
+    {id:10, type:'hardblock-h',  left:96, bottom:350},
+    {id:11, type:'hardblock-h',  left:192, bottom:350},
+    {id:12, type:'hardblock-h',  left:288, bottom:350},
+    {id:13, type:'hardblock-h',  left:384, bottom:350},
+    {id:14, type:'hardblock-v',  left:448, bottom:382},
+
+    {id:99, type:'platform',     h:10, w:96, left:650, bottom:350, animate:{keys:[{transform:'translateX(-50px)'},{transform:'translateX(50px)'}], duration:2000} }
   ])
 
   window.game.addPlayer(froggy);
