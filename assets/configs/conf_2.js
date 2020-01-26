67f4546d2cc0b9cfb9ebba2eb78ac3310806f696
Alex Meyer <129462+ameyerfr@users.noreply.github.com>
2020-01-26 19:13:42 +0100

    Update conf 2

--- assets/configs/conf_2.js
+++ assets/configs/conf_2.js
@@ -10,7 +10,39 @@ var gameConfig = {
       name:'mushroom',
       hurtbox : { top:24, left:16, width:32, height:38 },
       initialPosition: { left:250, bottom:126 },
-      botMove : { goLeft:50, goRight:50, offset:100, firstMove:'left' },
+      botMove : { goLeft:50, goRight:50, offset:100, firstMove:'right' },
+      reverseSpriteDir:true,
+      jumpHeight:15
+    },
+    {
+      name:'mushroom',
+      hurtbox : { top:24, left:16, width:32, height:38 },
+      initialPosition: { left:750, bottom:0 },
+      botMove : { goLeft:400, goRight:400, offset:800, firstMove:'left' },
+      reverseSpriteDir:true,
+      jumpHeight:15
+    },
+    {
+      name:'mushroom',
+      hurtbox : { top:24, left:16, width:32, height:38 },
+      initialPosition: { left:1700, bottom:0 },
+      botMove : { goLeft:200, goRight:200, offset:400, firstMove:'right' },
+      reverseSpriteDir:true,
+      jumpHeight:15
+    },
+    {
+      name:'mushroom',
+      hurtbox : { top:24, left:16, width:32, height:38 },
+      initialPosition: { left:300, bottom:750 },
+      botMove : { goLeft:200, goRight:200, offset:400, firstMove:'left' },
+      reverseSpriteDir:true,
+      jumpHeight:15
+    },
+    {
+      name:'mushroom',
+      hurtbox : { top:24, left:16, width:32, height:38 },
+      initialPosition: { left:900, bottom:750 },
+      botMove : { goLeft:200, goRight:200, offset:400, firstMove:'right' },
       reverseSpriteDir:true,
       jumpHeight:15
     }
@@ -47,32 +79,56 @@ var gameConfig = {
     {type:'hardblock-h',  left:436, bottom:406},
     {type:'hardblock-h',  left:342, bottom:406},
 
-    // {type:'hardblock-64',  left:0, bottom:408},
+    {type:'hardblock-64',  left:530, bottom:280},
 
+    {type:'hardblock-v',  left:1250, bottom:0},
+    {type:'hardblock-v',  left:1444, bottom:0},
+    {type:'hardblock-v',  left:1444, bottom:94},
 
-    // {type:'hardblock-h',  left:96, bottom:152},
-    // {type:'hardblock-h',  left:192, bottom:184},
-    // {type:'hardblock-h',  left:286, bottom:184},
-    // {type:'hardblock-h',  left:380, bottom:184},
-    // {type:'hardblock-32',  left:380, bottom:214},
-    //
-    // {type:'hardblock-32',  left:608, bottom:184},
-    // {type:'hardblock-h',  left:638, bottom:184},
-    //
-    // {type:'hardblock-h',  left:0, bottom:350},
-    // {type:'hardblock-32',  left:64, bottom:380},
-    // {type:'hardblock-32',  left:188, bottom:380},
-    // {type:'hardblock-h',  left:188, bottom:350},
-    // {type:'hardblock-h',  left:282, bottom:350},
-    // {type:'hardblock-h',  left:374, bottom:350},
-    // {type:'hardblock-v',  left:438, bottom:380},
-    // {type:'hardblock-64',  left:468, bottom:412},
-    //
-    // {type:'hardblock-64',  bottom:0, right:0},
+    {type:'hardblock-h',  left:1098, bottom:158},
+    {type:'hardblock-h',  left:1192, bottom:188},
+    {type:'hardblock-h',  left:1286, bottom:188},
+    {type:'hardblock-h',  left:1380, bottom:188},
+    {type:'hardblock-32',  left:1380, bottom:218},
 
-    {id:99, type:'platform',     h:10, w:96, left:650, bottom:350, animate:{keys:[{transform:'translateX(-50px)'},{transform:'translateX(50px)'}], duration:2000} },
-    {id:99, type:'platform',     h:10, w:96, left:750, bottom:450, animate:{keys:[{transform:'translateX(50px)'},{transform:'translateX(-50px)'}], duration:3000} },
-    {id:99, type:'platform',     h:10, w:96, left:550, bottom:480, animate:{keys:[{transform:'translateX(50px)'},{transform:'translateX(-50px)'}], duration:4000} }
+    {type:'hardblock-32',  left:1608, bottom:188},
+    {type:'hardblock-h',  left:1638, bottom:188},
+
+    {type:'hardblock-h',  left:1000, bottom:350},
+    {type:'hardblock-32',  left:1064, bottom:380},
+    {type:'hardblock-32',  left:1188, bottom:380},
+    {type:'hardblock-h',  left:1188, bottom:350},
+    {type:'hardblock-h',  left:1282, bottom:350},
+    {type:'hardblock-h',  left:1374, bottom:350},
+    {type:'hardblock-v',  left:1438, bottom:380},
+    {type:'hardblock-64',  left:1468, bottom:412},
+
+    {type:'hardblock-32',  left:1408, bottom:474},
+    {type:'hardblock-32',  left:1378, bottom:504},
+    {type:'hardblock-32',  left:1348, bottom:534},
+    {type:'hardblock-32',  left:1318, bottom:564},
+    {type:'hardblock-32',  left:1288, bottom:594},
+    {type:'hardblock-32',  left:1258, bottom:624},
+    {type:'hardblock-32',  left:1228, bottom:654},
+    {type:'hardblock-32',  left:1198, bottom:684},
+
+    {type:'hardblock-h',  left:1104, bottom:684},
+    {type:'hardblock-h',  left:1010, bottom:684},
+    {type:'hardblock-h',  left:916, bottom:684},
+    {type:'hardblock-h',  left:822, bottom:684},
+    {type:'hardblock-h',  left:728, bottom:684},
+    {type:'hardblock-h',  left:634, bottom:684},
+    {type:'hardblock-h',  left:540, bottom:684},
+    {type:'hardblock-h',  left:446, bottom:684},
+    {type:'hardblock-h',  left:352, bottom:684},
+    {type:'hardblock-h',  left:258, bottom:684},
+    {type:'hardblock-h',  left:164, bottom:684},
+    {type:'hardblock-h',  left:70, bottom:684},
+
+    {id:90, type:'platform',     h:10, w:96, left:850, bottom:170, animate:{keys:[{transform:'translateX(50px)'},{transform:'translateX(-200px)'}], duration:5000} },
+    {id:91, type:'platform',     h:10, w:96, left:850, bottom:330, animate:{keys:[{transform:'translateX(-200px)'},{transform:'translateX(50px)'}], duration:5000} },
+    {id:92, type:'platform',     h:10, w:96, left:1700, bottom:350, animate:{keys:[{transform:'translateX(-100px)'},{transform:'translateX(100px)'}], duration:5000} },
+    {id:92, type:'platform',     h:10, w:96, left:850, bottom:490, animate:{keys:[{transform:'translateX(50px)'},{transform:'translateX(-200px)'}], duration:5000} }
   ],
 
   items : [
@@ -91,6 +147,28 @@ var gameConfig = {
 
     { type:'shuriken', left: 450, bottom: 210 },
 
-    { type:'shuriken', left: 300, bottom: 30 }
+    { type:'shuriken', left: 300, bottom: 30 },
+    { type:'shuriken', left: 500, bottom: 30 },
+    { type:'shuriken', left: 700, bottom: 30 },
+    { type:'shuriken', left: 900, bottom: 30 },
+    { type:'shuriken', left: 1100, bottom: 30 },
+    { type:'shuriken', left: 1350, bottom: 30 },
+    { type:'shuriken', left: 1670, bottom: 30 },
+
+    { type:'shuriken', left: 650, bottom: 230 },
+    { type:'shuriken', left: 950, bottom: 230 },
+
+    { type:'shuriken', left: 500, bottom: 600 },
+    { type:'shuriken', left: 700, bottom: 600 },
+    { type:'shuriken', left: 900, bottom: 600 },
+
+    { type:'shuriken', left: 300, bottom: 1000 },
+    { type:'shuriken', left: 600, bottom: 1000 },
+    { type:'shuriken', left: 900, bottom: 1000 },
+
+    { type:'shuriken', left: 1350, bottom: 400 },
+    { type:'shuriken', left: 1800, bottom: 400 }
+
+
   ]
 }
